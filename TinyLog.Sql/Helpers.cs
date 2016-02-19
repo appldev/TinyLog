cac94f2d3dedb8de6071d7b5f404d69675256af5
Michael Randrup <mra@applications.dk>
2016-02-19 19:23:19 +0100

    Update SQL table and insert statement to LogEntry schema 1.2

--- TinyLog.Sql/Helpers.cs
+++ TinyLog.Sql/Helpers.cs
@@ -9,31 +9,33 @@ namespace TinyLog.Sql
     {
         public static SqlCommand GetReadOneCommand(string tableName)
         {
-            SqlCommand cmd = new SqlCommand(string.Format("SELECT TOP 1 * FROM {0} WITH(NOLOCK)", tableName));
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT TOP 1 Id FROM {0} WITH(NOLOCK)", tableName));
             return cmd;
         }
         public static SqlCommand GetInsertCommand(LogEntry logEntry, string table, SqlConnection con)
         {
             StringBuilder sql = new StringBuilder();
-            sql.AppendFormat("INSERT  INTO  {0}(Id, CorrelationId,CreatedOn, Title, Message, Source, Area, Client, ClientInfo, Severity, CustomDataFormatter, CustomData)", table);
-            sql.AppendLine("VALUES (@Id, @CorrelationId, @CreatedOn, @Title, @Message, @Source, @Area, @Client, @ClientInfo, @Severity, @CustomDataFormatter, @CustomData)");
+            sql.AppendFormat("INSERT  INTO  {0}(Id, CorrelationId,CreatedOn, Title, Message, Source, Area, Client, ClientInfo, Severity, CustomDataFormatter, CustomData, Signature, SignatureMethod)", table);
+            sql.AppendLine("VALUES (@Id, @CorrelationId, @CreatedOn, @Title, @Message, @Source, @Area, @Client, @ClientInfo, @Severity, @CustomDataFormatter, @CustomData, Signature, SignatureMethod)");
             SqlCommand cmd = new SqlCommand(sql.ToString(), con);
             cmd.Parameters.Add(GetParameter(cmd, "Id", SqlDbType.UniqueIdentifier, -1, logEntry.Id));
             cmd.Parameters.Add(GetParameter(cmd, "CorrelationId", SqlDbType.UniqueIdentifier, -1, logEntry.CorrelationId.HasValue ? (object)logEntry.CorrelationId.Value : (object)DBNull.Value));
             cmd.Parameters.Add(GetParameter(cmd, "CreatedOn", SqlDbType.DateTimeOffset, -1, logEntry.CreatedOn));
-            cmd.Parameters.Add(GetParameter(cmd, "Title", SqlDbType.NVarChar, 200, DbStr(logEntry.Title)));
-            cmd.Parameters.Add(GetParameter(cmd, "Message", SqlDbType.NVarChar, -1, DbStr(logEntry.Message)));
-            cmd.Parameters.Add(GetParameter(cmd, "Source", SqlDbType.NVarChar, 50, DbStr(logEntry.Source)));
-            cmd.Parameters.Add(GetParameter(cmd, "Area", SqlDbType.NVarChar, 50, DbStr(logEntry.Area)));
-            cmd.Parameters.Add(GetParameter(cmd, "Client", SqlDbType.NVarChar, 200, DbStr(logEntry.Client)));
-            cmd.Parameters.Add(GetParameter(cmd, "ClientInfo", SqlDbType.NVarChar, -1, DbStr(logEntry.ClientInfo)));
+            cmd.Parameters.Add(GetParameter(cmd, "Title", SqlDbType.NVarChar, 200, ToDbNull(logEntry.Title)));
+            cmd.Parameters.Add(GetParameter(cmd, "Message", SqlDbType.NVarChar, -1, ToDbNull(logEntry.Message)));
+            cmd.Parameters.Add(GetParameter(cmd, "Source", SqlDbType.NVarChar, 50, ToDbNull(logEntry.Source)));
+            cmd.Parameters.Add(GetParameter(cmd, "Area", SqlDbType.NVarChar, 50, ToDbNull(logEntry.Area)));
+            cmd.Parameters.Add(GetParameter(cmd, "Client", SqlDbType.NVarChar, 200, ToDbNull(logEntry.Client)));
+            cmd.Parameters.Add(GetParameter(cmd, "ClientInfo", SqlDbType.NVarChar, -1, ToDbNull(logEntry.ClientInfo)));
             cmd.Parameters.Add(GetParameter(cmd, "Severity", SqlDbType.NVarChar, 20, logEntry.SeverityString));
-            cmd.Parameters.Add(GetParameter(cmd, "CustomDataFormatter", SqlDbType.NVarChar, 100, DbStr(logEntry.CustomDataFormatter)));
-            cmd.Parameters.Add(GetParameter(cmd, "CustomData", SqlDbType.NVarChar, -1, DbStr(logEntry.CustomData)));
+            cmd.Parameters.Add(GetParameter(cmd, "CustomDataFormatter", SqlDbType.NVarChar, 100, ToDbNull(logEntry.CustomDataFormatter)));
+            cmd.Parameters.Add(GetParameter(cmd, "CustomData", SqlDbType.NVarChar, -1, ToDbNull(logEntry.CustomData)));
+            cmd.Parameters.Add(GetParameter(cmd, "Signature", SqlDbType.VarChar, 64, ToDbNull(logEntry.Signature)));
+            cmd.Parameters.Add(GetParameter(cmd, "SignatureMethod", SqlDbType.VarChar, 10, ToDbNull(logEntry.SignatureMethod)));
             return cmd;
         }
 
-        private static object DbStr(string s)
+        private static object ToDbNull(object s)
         {
             if (s == null)
             {
--- TinyLog/LogEntry.cs
+++ TinyLog/LogEntry.cs
@@ -43,7 +43,7 @@ namespace TinyLog
 
         #endregion
 
-        #region static methods
+        #region static methods and properties
 
         /// <summary>
         /// Makes a copy of a log entry object
