da249a6cb0a5dfd7d5c1e33ce795bad501954ff9
Michael Randrup <mra@applications.dk>
2016-02-19 18:09:34 +0100

    updated website with TinyLog-Mvc version 1.2.0-beta2

--- TinyLog.MvcTestWebsite/App_Start/TinyLogConfig.cs
+++ TinyLog.MvcTestWebsite/App_Start/TinyLogConfig.cs
@@ -12,7 +12,7 @@ namespace TinyLog.MvcTestWebsite
         public static void Initialize()
         {
             string ConnectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=TinyLogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            Log.Default.RegisterLogWriter(new SqlLogWriter(ConnectionString));
+            Log.Default.RegisterLogWriter(new LazyLogWriter(new SqlLogWriter(ConnectionString),15000));
             Log.Default.RegisterLogFormatter(new JsonSerializationFormatter());
             Log.Default.RegisterLogFormatter(new ActionFilterLogFormatter());
             Log.Default.RegisterLogFormatter(new ExceptionFormatter());
