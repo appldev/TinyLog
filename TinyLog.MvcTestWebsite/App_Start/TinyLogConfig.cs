5aacf96aacfc03e0502adb858cc3577b3b60c9b2
Michael Randrup <mra@applications.dk>
2016-03-02 09:24:28 +0100

    bumbed versions to v1.2

--- TinyLog.MvcTestWebsite/App_Start/TinyLogConfig.cs
+++ TinyLog.MvcTestWebsite/App_Start/TinyLogConfig.cs
@@ -12,7 +12,8 @@ namespace TinyLog.MvcTestWebsite
     {
         public static void Initialize()
         {
-            string ConnectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=TinyLogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            // string ConnectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=TinyLogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             Log.Default.RegisterLogWriter(new LazyLogWriter(new SqlLogWriter(ConnectionString),15000));
             Log.Default.RegisterLogFormatter(new JsonSerializationFormatter());
             Log.Default.RegisterLogFormatter(new ActionFilterLogFormatter());
