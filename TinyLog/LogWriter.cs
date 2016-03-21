30b3108ef3898c9d8c2a70d74106c9c6548ef435
Michael Randrup <mra@applications.dk>
2016-03-21 16:55:02 +0100

    Associates #30

--- TinyLog/LogWriter.cs
+++ TinyLog/LogWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace TinyLog
@@ -23,6 +24,57 @@ namespace TinyLog
         /// <param name="writeException">If the writer returns false, the writeException contains the error information</param>
         /// <returns>returns true if the log was sucessfully committed to the backend storage</returns>
         public abstract bool TryWriteLogEntry(LogEntry logEntry, out Exception writeException);
+
+        /// <summary>
+        /// Writes log entries as a batch. If this method is not overridden in a derived class, the log entries will be written using the WriteLogEntry() method
+        /// </summary>
+        /// <param name="logEntries">The log entries to write</param>
+        /// <param name="writeExceptions">An aggregate exception with one inner exception for each error encountered</param>
+        /// <returns>returns true if all log entries in the batch was written succesfully, otherwise false</returns>
+        public virtual bool TryWriteLogEntryBatch(LogEntry[] logEntries, out AggregateException writeExceptions)
+        {
+            List<Exception> exceptions = new List<Exception>();
+            foreach (LogEntry logEntry in logEntries)
+            {
+                Exception ex = null;
+                if (!TryWriteLogEntry(logEntry, out ex))
+                {
+                    exceptions.Add(ex);    
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                writeExceptions = new AggregateException(string.Format("{0} of {1} Log entries not written. See inner exceptions for details.", exceptions.Count, logEntries.Length), exceptions);
+                return false;
+            }
+            writeExceptions = null;
+            return true;
+        }
+
+        /// <summary>
+        /// Writes log entries as a batch.If this method is not overridden in a derived class, the log entries will be written using the WriteLogEntry() method
+        /// </summary>
+        /// <param name="logEntries">The log entries to write</param>
+        /// <returns>returns true if all log entries in the batch was written succesfully, otherwise false</returns>
+        public virtual Task<Tuple<bool, AggregateException>> TryWriteLogEntryBatchAsync(LogEntry[] logEntries)
+        {
+            List<Exception> exceptions = new List<Exception>();
+            foreach (LogEntry logEntry in logEntries)
+            {
+                Exception ex = null;
+                if (!TryWriteLogEntry(logEntry, out ex))
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                AggregateException writeExceptions = new AggregateException(string.Format("{0} of {1} Log entries not written. See inner exceptions for details.", exceptions.Count, logEntries.Length), exceptions);
+                return Task.FromResult<Tuple<bool, AggregateException>>(new Tuple<bool, AggregateException>(false, writeExceptions));
+            }
+            return Task.FromResult<Tuple<bool, AggregateException>>(new Tuple<bool, AggregateException>(true, null));
+        }
+
 
         /// <summary>
         /// Writes a log entry to the backend storage
